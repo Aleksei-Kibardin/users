c00eb1df704e7b8dec2f751a2ff12e3eaf7c0db9
Aleksei-Kibardin <kibardin20000@gmail.com>
2023-12-18 14:11:22 +0300

    test

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,14 @@
       <nav class="nav">
         <a class="back" href="/users/#/" v-if="$route.path !== '/'">Back</a>
         <h1 class="page-name">{{ $route.name }}</h1>
-        <search-users v-if="$route.path === '/'"></search-users>
+        <form class="search-users">
+    <input
+      :value="search"
+      @input="search = $event.target.value; store.commit('uploadsearchValue', search);"
+      type="text"
+      placeholder="Search by nickname..."
+    />
+  </form>
       </nav>
     </header>
     <main>
@@ -16,10 +23,11 @@
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useStore } from 'vuex';
 import { getUsers } from './api';
-import searchUsers from './components/search.vue'
+
+const search = ref("")
 
 const store = useStore();
 
@@ -72,5 +80,29 @@ main {
 .back {
   position: absolute;
   left: 20px;
+}
+.search-users {
+  width: auto;
+  float: right;
+  margin-right: 30px;
+  @media screen and (max-width: 300px) {
+    margin-right: 0px;
+  }
+
+  input {
+    color: #324b4e;
+    @include fluid("width", 350);
+    @include fluid("height", 62);
+    padding-left: 15px;
+    border-radius: 42px;
+    border: 2px solid #324b4e;
+    background: #f9f0da;
+    outline: none;
+    position: relative;
+    transition: 0.3s linear;
+  }
+  input:focus {
+    @include fluid("width", 400);
+  }
 }
 </style>
